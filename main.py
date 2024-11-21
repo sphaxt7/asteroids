6c6db256cf2c68490de2748d4da62999b9b37b96
sphaxt7 <issamtoxico@gmail.com>
2024-11-21 04:02:21 +0100

    badaboom

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import pygame
+from constants import *
+
+
 
 def main():
+    pygame.init()
     print("Starting asteroids!")
+    print("Screen width: 1280\nScreen height: 720")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 
