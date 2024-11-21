01471634e496cec890dbc398dce29daa319f72be
sphaxt7 <issamtoxico@gmail.com>
2024-11-21 16:01:52 +0100

    nahhh for real

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill(color="black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
+
+        #limiting fps to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
