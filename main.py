4cbdf74322fc05c203cb187360c81a3df8161f26
sphaxt7 <issamtoxico@gmail.com>
2024-11-21 14:15:29 +0100

    just stayin out the way

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import pygame
 from constants import *
 
 
-
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print("Screen width: 1280\nScreen height: 720")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(color="black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
